3215351cb203c1f679fd5391f8546a550e6e21f5
Ben Thorner <benthorner@outlook.com>
2018-04-05 23:40:41 +0100

    Add selector for Fake/Real data

--- src/application.js
+++ src/application.js
@@ -3,6 +3,9 @@ $(document).ready(function() {
   var layoutTaxadioNode = d3.select("#layout-taxadio").node()
   var layoutTaxadio = new Taxadio(layoutTaxadioNode, layoutOptions)
 
+  var dataTaxadioNode = d3.select("#data-taxadio").node()
+  var dataTaxadio = new Taxadio(dataTaxadioNode, ["Fake", "Real"])
+
   var taxitorNode = d3.select("#taxitor").node()
   var taxitor = new Taxitor(taxitorNode)
 
@@ -10,6 +13,16 @@ $(document).ready(function() {
     taxitor.trigger("layoutSelected", d)
   })
 
+  dataTaxadio.on("click", function(d) {
+    switch(d) {
+      case "Fake":
+        taxitor.trigger("dataReceived", new Taxode.Fake())
+        break
+      case "Real":
+        taxitor.trigger("dataReceived", Taxode.Real.root)
+        break
+    }
+  })
+
   taxitor.trigger("dataReceived", new Taxode.Fake())
-  //taxitor.trigger("dataReceived", Taxode.Real.root)
 })
