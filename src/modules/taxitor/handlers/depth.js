5de12f97219425b3f3a0a8dcbb2219b93b030a92
Ben Thorner <benthorner@outlook.com>
2018-04-09 23:03:03 +0100

    Use a specified maxHeight for the depth handler
    In the future the height of the tree may change without the leaf nodes
    exiting (e.g. if their parent changes), so this handler must be able to
    set/unset all potential classes a node may have (had).

--- src/modules/taxitor/handlers/depth.js
+++ src/modules/taxitor/handlers/depth.js
@@ -1,10 +1,11 @@
 Taxitor.Handlers.Depth = function(editor) {
   this.editor = editor
+  this.maxDepth = 9
   this.editor.on("onUpdate", this.onUpdate, this)
 }
 
 Taxitor.Handlers.Depth.prototype.onUpdate = function() {
-  for (var i=0; i<=this.editor.data.height; i++) {
+  for (var i=0; i<=this.maxDepth; i++) {
     this.editor.g
       .selectAll(".node")
       .classed("depth"+i, function(d) { return d.depth == i })
