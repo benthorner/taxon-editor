08df27ab00ac56aa15436d6d9cd0d7cfd567ca36
Ben Thorner <benthorner@outlook.com>
2018-04-14 17:06:22 +0100

    Automate calculate max depth in handler

--- src/modules/taxitor/handlers/depth.js
+++ src/modules/taxitor/handlers/depth.js
@@ -1,10 +1,13 @@
 export function DepthHandler(editor) {
   this.editor = editor
-  this.maxDepth = 9
+  this.maxDepth = 0
   this.editor.on("onUpdate", this.onUpdate, this)
 }
 
 DepthHandler.prototype.onUpdate = function() {
+  this.maxDepth = _.max([this.maxDepth,
+                         this.editor.data.height])
+
   for (var i=0; i<=this.maxDepth; i++) {
     this.editor.g
       .selectAll(".node")
