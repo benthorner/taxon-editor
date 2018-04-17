c54842a3fc8debf6311059b6ecc3891a31ad3148
Ben Thorner <benthorner@outlook.com>
2018-04-17 23:21:47 +0100

    Finish converting to ES6 classes

--- src/modules/taxitor/taxitor.js
+++ src/modules/taxitor/taxitor.js
@@ -1,26 +1,29 @@
 import {MainStage} from './stages/main.js'
 import {MainHandler} from './handlers/main.js'
 
-export function Taxitor(element) {
-  _.extend(this, Backbone.Events)
-  this.element = element
-  this.options = Taxitor.OPTIONS
-
-  this.g = d3
-    .select(element)
-    .classed("taxitor", true)
-    .append("g")
-
-  new MainStage(this)
-  new MainHandler(this)
-
-  this.on("dataReceived", function(data) {
-    this.data = data
-    this.trigger("beforeEnter")
-  })
-}
-
-Taxitor.OPTIONS = {
+const options = {
   ForceLayout: { simulationTicks: 1000, collisionRadius: 100 },
   ZoomHandler: { maxScaleFactor: 2, transformDelay: 500 }
 }
+
+export class Taxitor {
+  constructor(element) {
+    _.extend(this, Backbone.Events)
+    this.element = element
+    this.options = options
+
+    this.g = d3
+      .select(element)
+      .classed("taxitor", true)
+      .append("g")
+
+    new MainStage(this)
+    new MainHandler(this)
+
+    this.on("dataReceived", function(data) {
+      this.data = data
+      this.trigger("beforeEnter")
+    })
+  }
+}
+
--- src/modules/taxplay.js
+++ src/modules/taxplay.js
@@ -1,18 +1,20 @@
-export function Taxplay(element) {
-  _.extend(this, Backbone.Events)
-  this.element = d3.select(element)
-  this.on("dataReceived", this.dataReceived, this)
+export class Taxplay {
+  constructor(element) {
+    _.extend(this, Backbone.Events)
+    this.element = d3.select(element)
+    this.on("dataReceived", this.dataReceived, this)
 
-  this.element.classed("taxplay", true)
-  this.element.append("div").classed("title", true)
-  this.element.append("div").classed("base-path", true)
-  this.element.append("div").classed("id", true)
-  this.element.append("div").classed("description", true)
-}
+    this.element.classed("taxplay", true)
+    this.element.append("div").classed("title", true)
+    this.element.append("div").classed("base-path", true)
+    this.element.append("div").classed("id", true)
+    this.element.append("div").classed("description", true)
+  }
 
-Taxplay.prototype.dataReceived = function(d) {
-  this.element.select(".title").html(d.title)
-  this.element.select(".base-path").html(d.base_path)
-  this.element.select(".id").html(d.id)
-  this.element.select(".description").html(d.description)
+  dataReceived(d) {
+    this.element.select(".title").html(d.title)
+    this.element.select(".base-path").html(d.base_path)
+    this.element.select(".id").html(d.id)
+    this.element.select(".description").html(d.description)
+  }
 }
